b06805aa4cee6c01d4678d9eec17ef7b1c7b5512
Yaswanth <kosuariyaswanthkumar1903@gmail.com>
2026-03-11 21:20:49 +0530

    Ultra update one

--- script.js
+++ script.js
@@ -1,64 +1,66 @@
-const video = document.getElementById("video")
-const result = document.getElementById("result")
+const video = document.getElementById("video");
+const result = document.getElementById("result");
 
-Promise.all([
-faceapi.nets.tinyFaceDetector.loadFromUri('Models'),
-faceapi.nets.faceLandmark68Net.loadFromUri('Models'),
-faceapi.nets.faceRecognitionNet.loadFromUri('Models')
-]).then(startCamera)
-
-function startCamera(){
-navigator.mediaDevices.getUserMedia({video:{}})
+// START CAMERA FIRST
+navigator.mediaDevices.getUserMedia({ video: true })
 .then(stream => {
-video.srcObject = stream
+    video.srcObject = stream;
 })
-.catch(err => {
-result.innerText = "Camera permission denied"
-})
-}
+.catch(() => {
+    result.innerText = "Camera permission denied";
+});
 
-async function loadStudents(){
+// LOAD FACE API MODELS
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('Models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('Models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('Models')
+]).then(startRecognition);
 
-const labels = ['yash','nikhil','charan']
+async function startRecognition(){
 
-return Promise.all(
+const labels = ['yash','nikhil','charan'];
+
+const labeledDescriptors = await Promise.all(
 labels.map(async label => {
 
-const img = await faceapi.fetchImage(`students/${label}.jpg`)
-const detections = await faceapi
-.detectSingleFace(img)
-.withFaceLandmarks()
-.withFaceDescriptor()
+    const img = await faceapi.fetchImage(`students/${label}.jpg`);
 
-return new faceapi.LabeledFaceDescriptors(label,[detections.descriptor])
+    const detections = await faceapi
+    .detectSingleFace(img)
+    .withFaceLandmarks()
+    .withFaceDescriptor();
+
+    return new faceapi.LabeledFaceDescriptors(label,[detections.descriptor]);
+
 })
-)
-}
+);
 
-video.addEventListener("play", async () => {
+const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors,0.6);
 
-const labeledDescriptors = await loadStudents()
-const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors,0.6)
+video.addEventListener("play", () => {
 
 setInterval(async () => {
 
 const detections = await faceapi
 .detectAllFaces(video,new faceapi.TinyFaceDetectorOptions())
 .withFaceLandmarks()
-.withFaceDescriptors()
+.withFaceDescriptors();
 
-if(detections.length>0){
+if(detections.length > 0){
 
-const bestMatch = faceMatcher.findBestMatch(detections[0].descriptor)
+const match = faceMatcher.findBestMatch(detections[0].descriptor);
 
-if(bestMatch.label !== "unknown"){
-result.innerText = bestMatch.label + " belongs to DCME-B"
+if(match.label !== "unknown"){
+result.innerText = match.label + " belongs to DCME-B";
 }else{
-result.innerText = "Face not recognised"
+result.innerText = "Face not recognised";
 }
 
 }
 
-},1000)
+},1000);
 
-})
+});
+
+}
